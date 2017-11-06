0857eeb968c0315fe41b153034c9c45716524858
Shekhar Kumar <shekhar.kumar@acando.no>
2017-11-07 00:10:54 +0100

    config changes for postgresql

--- src/main/java/edu/shekhar/blog/dao/PostgresqlBlogDaoImpl.java
+++ src/main/java/edu/shekhar/blog/dao/PostgresqlBlogDaoImpl.java
@@ -2,6 +2,7 @@ package edu.shekhar.blog.dao;
 
 import edu.shekhar.blog.entity.Blog;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
 import org.springframework.jdbc.core.support.JdbcDaoSupport;
